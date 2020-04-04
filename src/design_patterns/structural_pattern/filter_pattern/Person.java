6175e513aa1d047b8601f8aadf261f7a4822181f
popany <popany@163.com>
2020-04-04 21:55:04 +0000

    add

---
+++ src/design_patterns/structural_pattern/filter_pattern/Person.java
@@ -0,0 +1,25 @@
+package design_patterns.structural_pattern.filter_pattern;
+
+public class Person {
+	private String name;
+	private String gender;
+	private String maritalStatus;
+	
+	public Person(String name, String gender, String maritalStatus) {
+		this.name = name;
+		this.gender = gender;
+		this.maritalStatus = maritalStatus;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+}
